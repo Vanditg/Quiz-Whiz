0f89832a67c21b5bde4f87a9f2113453f34d0478
Vandit Gajjar <gajjarvandit@gmail.com>
2023-03-21 22:58:23 +1030

    Push: Quiz Whiz HTML/CSS/JS scripts

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Quiz Whiz
- Quiz-Whiz is a quiz application platform, where users can practice, improve, and test their knowledge skills for exams. During the examination, users will be presented with a range of questions with 1 minute to answer. 
+ Quiz-Whiz is a quiz application platform, where users can practice, improve, and test their knowledge skills for exams. During the examination, users will be presented with a range of questions with 15 seconds per questions to answer. 
---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How can Salesforce Admin ensure that all Salesforce data is backed up every week?",
+    answer: "Weekly Data Export",
+    options: [
+      "Import Wizard",
+      "Apex Data Loader",
+      "Export Wizard",
+      "Weekly Data Export"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Email Templates folders in Salesforce can grant permissions to different sets of users. Which feature is not available when sharing Email Template folders?",
+    answer: "Profiles",
+    options: [
+      "Public Groups",
+	  "Roles",
+	  "Roles and Subordinates",
+      "Profiles"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Universal Container sells both classic and lightning products to its customers. What should the Salesforce Admin use to categorize the company product?",
+    answer: "Product family",
+    options: [
+      "Product family",
+      "Product Classic",
+      "Product Lightning",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following objects shows the activity timeline in Salesforce?",
+    answer: "Opportunities",
+    options: [
+      "Opportunities",
+      "My personal settings",
+      "User Setup",
+      "Users"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Universal Container’s Sales reps were storing information about individual people who is a consumer in their business. Which type of Salesforce account are they using?",
+    answer: "Person Account",
+    options: [
+      "Person Account",
+      "Business Account",
+      "Group Account",
+      "Customer Account"
+    ]
+  },
+];
